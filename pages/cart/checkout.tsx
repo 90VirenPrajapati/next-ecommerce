ecda92140121ea07941c39caeeed1479eb0fa317
viren <viren.viru.19@gmail.com>
2024-04-02 18:07:51 +0530

    -m

--- pages/cart/checkout.tsx
+++ pages/cart/checkout.tsx
@@ -17,7 +17,7 @@ const CheckoutPage = () => {
 
     return (
         <Layout>
-            <section className="cart">
+            <section className="cart" >
                 <div className="container">
                     <div className="cart__intro">
                         <h3 className="cart__title">Shipping and Payment</h3>
