eea8e1b163424a6341dc4d8f55b7f99f56b08a19
viren <viren.viru.19@gmail.com>
2024-04-02 18:37:59 +0530

    -m

--- pages/cart/checkout.tsx
+++ pages/cart/checkout.tsx
@@ -4,6 +4,20 @@ import CheckoutStatus from '../../components/checkout-status';
 import CheckoutItems from '../../components/checkout/items';
 import {RootState} from 'store';
 
+const paymentMethods = [
+    {name: 'Paypal', src: '/images/logos/paypal.png'},
+    {name: 'Visa', src: '/images/logos/visa.png'},
+    {name: 'Mastercard', src: '/images/logos/mastercard.png'},
+    {name: 'Maestro', src: '/images/logos/maestro.png'},
+    {name: 'Discover', src: '/images/logos/discover.png'},
+    {name: 'Ideal', src: '/images/logos/ideal-logo.svg'}
+]
+const deliveryMethods = [
+    {name: 'InPost', src: '/images/logos/inpost.svg', price: '$20.00'},
+    {name: 'DPD', src: '/images/logos/dpd.svg', price: '$12.00'},
+    {name: 'DHL', src: '/images/logos/dhl.svg', price: '$15.00'},
+    {name: 'Maestro', src: '/images/logos/maestro.png', price: '$10.00'}
+]
 const CheckoutPage = () => {
 
     const priceTotal = useSelector((state: RootState) => {
@@ -17,20 +31,18 @@ const CheckoutPage = () => {
 
     return (
         <Layout>
-            <section className="cart" >
+            <section className="cart">
                 <div className="container">
                     <div className="cart__intro">
                         <h3 className="cart__title">Shipping and Payment</h3>
                         <CheckoutStatus step="checkout"/>
                     </div>
-
                     <div className="checkout-content">
                         <div className="checkout__col-6">
                             <div className="checkout__btns">
                                 <button className="btn btn--rounded btn--yellow">Log in</button>
                                 <button className="btn btn--rounded btn--border">Sign up</button>
                             </div>
-
                             <div className="block">
                                 <h3 className="block__title">Shipping information</h3>
                                 <form className="form">
@@ -39,25 +51,21 @@ const CheckoutPage = () => {
                                             <input className="form__input form__input--sm" type="text"
                                                    placeholder="Email" required/>
                                         </div>
-
                                         <div className="form__col">
                                             <input className="form__input form__input--sm" type="text"
                                                    placeholder="Address" required/>
                                         </div>
                                     </div>
-
                                     <div className="form__input-row form__input-row--two">
                                         <div className="form__col">
                                             <input className="form__input form__input--sm" type="text"
                                                    placeholder="First name" required/>
                                         </div>
-
                                         <div className="form__col">
                                             <input className="form__input form__input--sm" type="text"
                                                    placeholder="City" required/>
                                         </div>
                                     </div>
-
                                     <div className="form__input-row form__input-row--two">
                                         <div className="form__col">
                                             <input className="form__input form__input--sm" type="text"
@@ -69,29 +77,18 @@ const CheckoutPage = () => {
                                                    placeholder="Postal code / ZIP" required/>
                                         </div>
                                     </div>
-
                                     <div className="form__input-row form__input-row--two">
                                         <div className="form__col">
                                             <input className="form__input form__input--sm" type="text"
                                                    placeholder="Phone number" required/>
                                         </div>
-
                                         <div className="form__col">
                                             <div className="select-wrapper select-form">
                                                 <select>
                                                     <option>State</option>
-                                                    <option value="Argentina">Gujarat</option>
-                                                    <option value="Argentina">Rajasthan</option>
-                                                    <option value="Argentina">Punjab</option>
-                                                    <option value="Argentina">Haryana</option>
-                                                    <option value="Argentina">Delhi</option>
-                                                    <option value="Argentina">Mumbai</option>
-                                                    <option value="Argentina">Pune</option>
-                                                    <option value="Argentina">Chennai</option>
-                                                    <option value="Argentina">Bangalore</option>
-                                                    <option value="Argentina">Assam</option>
-                                                    <option value="Argentina">UP</option>
-                                                    <option value="Argentina">MP</option>
+                                                    {['Gujarat', 'Rajasthan', 'Punjab', 'Haryana', 'Delhi', 'Mumbai', 'Pune', 'Chennai', 'Bangalore', 'Assam', 'UP', 'MP'].map((state, index) => (
+                                                        <option key={index} value={state}>{state}</option>
+                                                    ))}
                                                 </select>
                                             </div>
                                         </div>
@@ -99,60 +96,33 @@ const CheckoutPage = () => {
                                 </form>
                             </div>
                         </div>
-
                         <div className="checkout__col-4">
                             <div className="block">
-                                <h3 className="block__title">Payment method</h3>
+                                <h3 className="block__title"> Payment method </h3>
                                 <ul className="round-options round-options--three">
-                                    <li className="round-item">
-                                        <img src="/images/logos/paypal.png" alt="Paypal"/>
-                                    </li>
-                                    <li className="round-item">
-                                        <img src="/images/logos/visa.png" alt="Paypal"/>
-                                    </li>
-                                    <li className="round-item">
-                                        <img src="/images/logos/mastercard.png" alt="Paypal"/>
-                                    </li>
-                                    <li className="round-item">
-                                        <img src="/images/logos/maestro.png" alt="Paypal"/>
-                                    </li>
-                                    <li className="round-item">
-                                        <img src="/images/logos/discover.png" alt="Paypal"/>
-                                    </li>
-                                    <li className="round-item">
-                                        <img src="/images/logos/ideal-logo.svg" alt="Paypal"/>
-                                    </li>
+                                    {paymentMethods.map((method, index) => (
+                                        <li key={index} className="round-item">
+                                            <img src={method.src} alt={method.name}/>
+                                        </li>
+                                    ))}
                                 </ul>
                             </div>
-
                             <div className="block">
                                 <h3 className="block__title">Delivery method</h3>
                                 <ul className="round-options round-options--two">
-                                    <li className="round-item round-item--bg">
-                                        <img src="/images/logos/inpost.svg" alt="Paypal"/>
-                                        <p>$20.00</p>
-                                    </li>
-                                    <li className="round-item round-item--bg">
-                                        <img src="/images/logos/dpd.svg" alt="Paypal"/>
-                                        <p>$12.00</p>
-                                    </li>
-                                    <li className="round-item round-item--bg">
-                                        <img src="/images/logos/dhl.svg" alt="Paypal"/>
-                                        <p>$15.00</p>
-                                    </li>
-                                    <li className="round-item round-item--bg">
-                                        <img src="/images/logos/maestro.png" alt="Paypal"/>
-                                        <p>$10.00</p>
-                                    </li>
+                                    {deliveryMethods.map((method, index) => (
+                                        <li key={index} className="round-item round-item--bg">
+                                            <img src={method.src} alt={method.name}/>
+                                            <p>{method.price}</p>
+                                        </li>
+                                    ))}
                                 </ul>
                             </div>
                         </div>
-
                         <div className="checkout__col-2">
                             <div className="block">
                                 <h3 className="block__title">Your cart</h3>
                                 <CheckoutItems/>
-
                                 <div className="checkout-total">
                                     <p>Total cost</p>
                                     <h3>${priceTotal}</h3>
@@ -160,7 +130,6 @@ const CheckoutPage = () => {
                             </div>
                         </div>
                     </div>
-
                     <div className="cart-actions cart-actions--checkout">
                         <a href="/cart" className="cart__btn-back"><i className="icon-left"></i> Back</a>
                         <div className="cart-actions__items-wrapper">
@@ -173,6 +142,5 @@ const CheckoutPage = () => {
         </Layout>
     )
 };
-
 
 export default CheckoutPage
